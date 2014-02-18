39777d72cb5a0007fac28ef64bd5fbe3157da0bb
Michael Contino <michael.contino@pondurance.com>
2014-02-17 19:53:15 -0500

    Fixed authinication crashes and SQLi possbility

--- auth.py
+++ auth.py
@@ -6,16 +6,28 @@ def authenhandler(req):
 
     pw = mysql_password(req.get_basic_auth_pw())
     user = req.user
+	
+    #check for null or none password
+	if pw == "*BE1BDEC0AA74B4DCB079943E70528096CCA985F8":
+        return apache.HTTP_UNAUTHORIZED
     
     #get the connection information for DB
     conn = Connect_To_Database()
     
     #open a connection to the DB server
     curs = conn.cursor()
+	
+	#clean user input
+	user = user.replace('"', "").replace("'", "").replace("-", "").replace("+", "").replace("=", "")
     
     #execute a check to see if
     curs.execute("SELECT User_Password FROM PS_Users WHERE User_Name =%s",(user))
     
+    #check for bad user
+    if mysql_pw == None:
+        curs.close()
+        return apache.HTTP_UNAUTHORIZED	
+	
     #catch the server response
     mysql_pw = curs.fetchone()
 

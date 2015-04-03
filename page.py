66e079daf6172288ea76ffb6f02548eb0f528956
Michael Contino <mcontino@live.com>
2015-04-03 03:15:23 -0400

    this is starting to look much better

--- page.py
+++ page.py
@@ -1,4 +1,4 @@
-from mod_python import Session, apache, util
+from mod_python import apache, Session
 from lib import *
 
 def index(req):
@@ -11,94 +11,105 @@ def index(req):
     except:
         user_name = ""
     challenges = session['challenges']
-
+ 
     if user_name == "":
-        req.write("<center><h2>Login to submit flags</h2></center>")
+         req.write("<center><h2>Login to submit flags</h2></center>")
 
     req.write('<link rel="stylesheet" href="/css/login.css">')
     req.write('<div class="container">')
 
-    #reset counter and total
+    req.write("<p><br>")
+    req.write("<center><h2>Challenges</h2></center>")
+    req.write('<div class="col-sm-4">')
+    req.write('<table class="table">')
+    req.write('<thead>')
+    req.write('<tr>')
+    req.write('<th>#</th>')
+    req.write('<th>Status</th>')
+    req.write('<th>Value</th>')
+    req.write('<th>Challenge Text</th>')
+    req.write('</tr>')
+    req.write('</thead>')
+    req.write('<tbody>')
+
+
     counter = 0
     total = 0
     for challenge in challenges:
+         counter += 1
+         total += int(str(challenge[1]).replace('L', ''))
 
-        #incrament counter and total
-        counter += 1
-        total += int(str(challenge[1]).replace('L', ''))
-        
-        #Display the challenge number and point value
-        req.write("<p>")
-        req.write("<center><h4>Challenge " + str(counter) + " worth " + str(challenge[1]).replace('L', '') + " points.</h4></center>")
+         req.write('<tr>')
+         req.write('<td>' + str(counter) + '</td>')
+         if user_name == "":
+             req.write('<td> Login </td>')
+         elif user_name in challenge[0].split(","):
+             req.write('<td> Complete </td>')
+         else:
+             req.write('<td> Open </td>')
+         req.write('<td>' + str(challenge[1]).replace('L', '') + '</td>')
+         req.write('<td><div style="width: 650px; height: 120px;">' + str(challenge[2]) + '</div></td>')
 
-        #indicate if the challenge has been complted 
-        if user_name == "":
-           req.write("")
+         if user_name not in challenge[0].split(","):
+             req.write('<form class="form-signin" action="page.py/challenge" method="POST">')
+             req.write('<td><div style="width: 150px; height: 15px;"><input type="text" name="answer" class="form-control" placeholder="Answer"><br></div></td>')
+             req.write('<td><input type="hidden" name="challengenum" value="' + str(counter) + '"></td>')
+             req.write('<td><div style="width: 80px; height: 15px;"><button class="btn btn-primary btn-block" type="submit">Submit</button></td>')
+             req.write("</form>")
 
-        elif user_name in challenge[0].split(","):
-            req.write("<p>")
-            req.write("<center>Status: Complete</center>")
 
-        else:
-            req.write("<p><html>")
-            req.write("<center><h3> Status: Open </h3></center>")
-            req.write('<form class="form-signin" action="page.py/challenge" method="POST">')
-            req.write('<input type="text" name="answer" class="form-control" placeholder="Answer"><br>')
-            req.write('<input type="hidden" name="challengenum" value="' + str(counter) + '">') 
-            req.write('<button class="btn btn-primary btn-block" type="submit">Submit</button>')
-            req.write("</form></html>")
-
-        #display the challenge text
-        req.write("<center><p>" + str(challenge[2]) + "</center>")
-
-    #indicate the end of challenges
-    req.write("<center><h2>End of Challenges</h2></center>")
-    req.write("<center><h3>" + str(counter) + " total challenges worth " + str(total) + " points.<h/3></center>")
-
+    req.write('</tr>')
+    req.write('</center></tbody>')
+    req.write('</table>')
+    req.write('</div>')
+    req.write('<div style="position: fixed; bottom: 0; left: 5px;">')
+    req.write("<center><h3>" + str(counter) + " total challenges worth " + str(total) + " points.</h3></center>")
+    req.write('</div>')
 
 def challenge(req, answer, challengenum):
 
-    session = Session.Session(req)
-    try:
-        user = session['login']
+     session = Session.Session(req)
+     try:
+          user = session['login']
 
-    except:
-        util.redirect(req, "/login")
+     except:
+          util.redirect(req, "/login")
 
 
-    #set content type to html
-    req.content_type = "text/html"
+     #set content type to html
+     req.content_type = "text/html"
 
-    #get a list of the challenges
-    challenges = session['challenges']
+     #get a list of the challenges
+     Get_Challenges(session)
+     challenges = session['challenges']
 
-    if user in challenges[int(challengenum) - 1][0].split(","):
-        session['msg'] = 8
-        session.save()
-        util.redirect(req, "/")
+     if user in challenges[int(challengenum) - 1][0].split(","):
+          session['msg'] = 8
+          session.save()
+          util.redirect(req, "/")
 
-    if challenges[int(challengenum) - 1][3] == answer:
+     if challenges[int(challengenum) - 1][3] == answer:
 
-        #get the connection information for DB
-        conn = Connect_To_Database()
+          #get the connection information for DB
+          conn = Connect_To_Database()
 
-        #open a connection to the DB server
-        curs = conn.cursor()
+          #open a connection to the DB server
+          curs = conn.cursor()
 
-        userlist = challenges[int(challengenum) - 1][0] + "," + user
-        #add the user to the completed challenge list
-        curs.execute("UPDATE PS_Challenges SET Challenge_Completed=%s WHERE Challenge_Answer=%s", (userlist, answer))
+          userlist = challenges[int(challengenum) - 1][0] + "," + user
+          #add the user to the completed challenge list
+          curs.execute("UPDATE PS_Challenges SET Challenge_Completed=%s WHERE Challenge_Answer=%s", (userlist, answer))
 
-        #update the total points earned
-        total = int(Get_Total_Points(session)) + int(challenges[int(challengenum) - 1][1])
-        curs.execute("UPDATE PS_Users SET Total_Points =%s WHERE User_Name =%s", (str(total), user))
+          #update the total points earned
+          total = int(Get_Total_Points(user)) + int(challenges[int(challengenum) - 1][1])
+          curs.execute("UPDATE PS_Users SET Total_Points =%s WHERE User_Name =%s", (str(total), user))
 
-        #commit and close connection to database
-        conn.commit()
-        curs.close()
-        util.redirect(req, "/")
+          #commit and close connection to database
+          conn.commit()
+          curs.close()
+          util.redirect(req, "/")
 
-    else:
-        session['msg'] = 7
-        session.save()
-        util.redirect(req, "/")		
+     else:
+          session['msg'] = 7
+          session.save()
+          util.redirect(req, "/")
